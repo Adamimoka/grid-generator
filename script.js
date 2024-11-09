93e52e5b04cda877be943658948d06a158051d54
Adamimoka <adamimoka@gmail.com>
2024-11-09 18:26:51 -0500

    added main code for square grid

--- script.js
+++ script.js
@@ -1,32 +1,24 @@
-function GenerateGrid() {
-    alert("Grid Created!");
-    document.getElementById("confirm-text").innerHTML = "Generated!";
-        // Step 1: Create a canvas element
-        const canvas = document.createElement("canvas");
-        canvas.width = 1200;  // Set desired width
-        canvas.height = 600; // Set desired height
-    
-        // Step 2: Get the drawing context of the canvas
-        const context = canvas.getContext("2d");
-    
-        // Step 3: Fill backgroud
-        context.fillStyle = "#000000";
-        context.fillRect(0, 0, canvas.width, canvas.height);
+function ChangeGridType() {
+    const gridType = +document.getElementById("grid-type-selector").value;
+    alert(gridType)
 
-        // Step 4: Set the fill color and draw a rectangle to fill the canvas
-        context.fillStyle = "#ff5733";  // Set your desired color (e.g., orange)
-        for (let x = 0; x < 5; x++) {
-            for (let y = 0; y < 5; y++) {
-                context.fillRect(x*100, y*100, 25, 25);
-            }
-        }
-    
-        // Step 5: Convert the canvas to a data URL and create an image element
-        const image = document.createElement("img");
-        image.src = canvas.toDataURL("image/png");  // Convert canvas to an image source
-    
-        // Step 6: Append the image to the container
-        const container = document.getElementById("image-container");
-        container.innerHTML = "";  // Clear existing content
-        container.appendChild(image);  // Add the new image
+    let settingsDivs = [
+        document.getElementById("square-grid-settings"),
+        document.getElementById("other-grid-settings")
+    ];
+    for (let index = 0; index < settingsDivs.length; index++) {
+        const settingsDiv = settingsDivs[index];
+        settingsDiv.style.visibility = "hidden";
+    }
+    settingsDivs[gridType].removeAttribute("hidden");
+    document.getElementById("generate-button").removeAttribute("hidden");
+
+    const container = document.getElementById("image-settings");
+    container.innerHTML = "";  // Clear existing content
+    container.appendChild(settings);  // Add the new image
+}
+
+function GenerateGrid() {
+    document.getElementById("confirm-text").innerHTML = "Grid Generated";
+    SquareGrid()
 }
